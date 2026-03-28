fd84a36aecf886fc5043322df4939c3c815f2af5
Sandeep Kumar <sandeep.kumar2025x@gmail.com>
2026-03-28 14:09:48 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -1,40 +1,20 @@
-import dotenv from 'dotenv';
 import connectDB from './db/index.js';
-
-
-
-dotenv.config({
-    path:'./env'
-})
-
-
-
-/*
 import express from 'express';
+
 const app = express();
 
+// 🔍 debug ke liye
+console.log("ENV CHECK:", process.env.MONGODB_URI);
 
-(async () => {
-    try {
-        await mongoose.connect(` ${process.env.MONGODB_URI}/${DB_NAME}`)
-        app.on("error", (error) => {
-            console.log("ERROR", error);
-            throw error ;
-            
-        })
-
-
-        app.listen(process.env.PORT, () => {
-            console.log(`App is listening on port ${process.env.PORT}`);
-            
-        })
-
-    } catch (error) {
-        console.error("ERROR", error);
-        throw error;
+// 🔥 DB connect
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT || 8000}`);
         
-        
-    }
-}) ();
-*/
-
+    })
+})
+.catch((err) => {
+    console.log('MONGO DB Connection failed !!! ' , err);
+    
+})
