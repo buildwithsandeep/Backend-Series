f39efb7b5067ae4564493f49f0074c7754dbb072
Sandeep Kumar <sandeep.kumar2025x@gmail.com>
2026-04-02 13:55:34 +0530

    all setup are configured

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,87 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/User.model.js";
+import {uploadOnCloudinary} from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: 'data received successfully',
-    });
+    // get user details from frontend
+    // validation - not empty
+    // if user already exists - username, email
+    // check for images, check for avatar, cover image
+    // upload them to cloudinary, avatar
+    // create user object - create entry in database
+    // remove password and refreshToken field from response
+    // check for user creation
+    // return response to frontend
+
+
+ const {fullName, username, email, password} = req.body
+
+ console.log({fullName, username, email, password});
+ 
+ if (
+    [fullName, username, email, password].some((field) => 
+        field?.trim() === "")
+ ) {
+    throw new ApiError("All fields are required", 400);
+ }
+
+ const existingUser = await User.findOne({
+    $or: [
+        {username},
+        {email}
+    ]   
+ })
+
+    if (existingUser) { 
+        throw new ApiError("User already exists", 400);
+    }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+
+    if (!avatarLocalPath) {
+        throw new ApiError("Avatar is required", 400);
+    }
+
+    if (!coverImageLocalPath) {
+        throw new ApiError("Cover image is required", 400);
+    }
+
+    const avatarUploadResponse = await uploadOnCloudinary(avatarLocalPath);
+    const coverImageUploadResponse = await uploadOnCloudinary(coverImageLocalPath);
+
+    if (!avatarUploadResponse) {
+        throw new ApiError("Avatar upload failed", 500);
+    }
+
+    if (!coverImageUploadResponse) {
+        throw new ApiError("Cover image upload failed", 500);
+    }
+
+   const user = await User.create({
+        fullName,
+        username: username.toLowerCase(),
+        email,
+        password,
+        avatar: avatarUploadResponse.url,
+        coverImage: coverImageUploadResponse.url
+    })
+
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken -__v"
+    )
+
+    if (!createdUser) {
+        throw new ApiError("User creation failed", 500);
+    }
+
+    res.status(201).json(new ApiResponse(
+        "User registered successfully",
+        createdUser
+    ))
 });
 
 export { registerUser };
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -21,7 +21,7 @@ const userSchema = new Schema(
             trim: true,
 
         },
-        fullname: {
+        fullName: {
             type: String,
             required: true,
             trim: true,
@@ -59,7 +59,7 @@ const userSchema = new Schema(
 userSchema.pre("save", async function (next) {
     if(!this.isModified("password")) return next();
 
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
@@ -73,7 +73,7 @@ userSchema.methods.generateAccessToken = function(){
             _id: this._id,
             email: this.email,
             username: this.username,
-            fullname: this.fullname
+            fullName: this.fullName
         },
         process.env.ACCESS_TOKEN_SECRET,
         {
